2ee1d001a3f1cf13c13e938e00928c8abfc40dde
choupijiang <zhangshenghu@gmail.com>
2015-01-03 22:14:18 +0800

    Merge branch 'master' of https://github.com/choupijiang/algorithm
    Conflicts:
    	src/linkedList/PositionalList.py

--- src/trees/LinkedBinaryTree.py
+++ src/trees/LinkedBinaryTree.py
@@ -133,6 +133,112 @@ class LinkedBinaryTree(BinaryTree):
             count+=1
         return count
 
+    def _add_root(self, e):
+        """
+        Place element e at the root of an empty tree and return new Postition.
+        Raise ValueError if tree nonempty.
+        :param e:
+        :return:
+        """
+        if self._root is not None:
+            raise ValueError("Root exists.")
+        self._size = 1
+        self._root = self._Node(e)
+        return self._make_position(self._root)
+
+    def _add_left(self, p, e):
+        """
+        Place element e at the root of an empty tree and return new Position.
+        Raise ValueError if tree nonempty.
+        :param p:
+        :param e:
+        :return:
+        """
+        node = self._validata(p)
+        if node._left  is not None:
+            raise ValueError("Left child exists.")
+        self._size += 1
+        node._left = self._Node(e, node)
+        return self._make_position(node._left)
+
+    def _add_right(self, p, e):
+        """
+        Create a new right child for Position p, storing element e.
+        Return the Position of new node.
+        Raise ValueError if Position p is invalid or p already has a right child.
+        :param p:
+        :param e:
+        :return:
+        """
+        node = self._validata(p)
+        if node._right is not None:
+            raise ValueError("Right child exists")
+        self._size += 1
+        node._right = self._Node(e, node)
+        return self._make_position(node._right)
+
+    def _replace(self, p, e):
+        """
+        Replace the element at position p with e, and return old element.
+        :param p:
+        :param e:
+        :return:
+        """
+        node = self._validata(p)
+        old =node._element
+        node._element = e
+        return old
+
+    def _delete(self, p):
+        """
+        Delete the node at Position p, and replace it with its child, if any.
+        Return the element that had been stored at Position p.
+        Raise ValueError if Position p is invalid or p has two children.
+        :param p:
+        :return:
+        """
+        node = self._validata(p)
+        if self.num_children(p) == 2:
+            raise ValueError("p has two children.")
+        child = node._left if node._left else node._right
+        if child is not None:
+            child._parent = node._parent
+        if node is self.root:
+            self._root = child
+        else:
+            parent = node._parent
+            if node is parent._left:
+                parent._left = child
+            else:
+                parent._right = child
+        self._size -= 1
+        node._parent = node
+        return node._element
+
+    def _attach(self, p, t1, t2):
+        """
+        Attach trees t1 and t2 as left and right subtrees of external p.
+        :param p:
+        :param t1:
+        :param t2:
+        :return:
+        """
+        node = self._validata(p)
+        if not self.is_leaf(p):
+            raise ValueError("Position must be leaf.")
+        if not type(self) is type(t1) is type(t2):
+            raise TypeError("Tree types must match.")
+        self._size += len(t1) + len(t2)
+        if not t1.is_empty():
+            t1._root._parent = node
+            node._left = t1._root
+            t1._root = None
+            t1._size = 0
+        if not t2.is_empty():
+            t2._root._parent = node
+            node._right = t2._root
+            t2._root = None
+            t2._size = 0
 
 
 
