455b579031de6e40078522adb6888130db12ce6b
choupijiang <zhangshenghu@gmail.com>
2015-01-06 12:50:01 +0800

    Priority Queue

---
+++ src/priorityQueue/PriorityQueueBase.py
@@ -0,0 +1,28 @@
+#! /use/env/bin python
+#-*- coding:utf-8 -*-
+
+class PriorityQueueBase:
+    """
+    Abstract base class for a priority queue.
+    """
+    class _Item:
+        """
+        Lightweight composite to store priority queue items.
+        """
+        __slots__ = '_key', '_value'
+
+        def __init__(self, k, v):
+            self._key = k
+            self._value = v
+
+        def __lt__(self, other):
+            return self._key < other._key
+
+    def is_empty(self):
+        """
+        Return True if the priority queue is empty.
+        :return:
+        """
+        return len(self) == 0
+
+
---
+++ src/priorityQueue/UnsortedPrioriyQueue.py
@@ -0,0 +1,71 @@
+#! /use/env/bin python
+#-*- coding:utf-8 -*-
+import  sys
+from PriorityQueueBase import PriorityQueueBase
+sys.path.append("../linkedList")
+from PositionalList import PositionalList
+
+class Empty(Exception):
+    pass
+
+
+class UnsortedPriorityQueue(PriorityQueueBase):
+    """
+    A min-oriented priority queue implemented with an unsorted list.
+    """
+
+    def __init__(self):
+        """
+        Create an empty Priority Queue.
+        :return:
+        """
+        self._data = PositionalList()
+
+    def _find_min(self):
+        """
+        Return Position of item with minimum key.
+        :return:
+        """
+        if self.is_empty():
+            raise Empty("Priority queue is empty.")
+        small = self._data.first()
+        walk = self._data.after(small)
+        while walk is not None:
+            if walk.element() < small.element():
+                small = walk
+            walk = self._data.after(walk)
+        return small
+
+    def min(self):
+        """
+        Return but do not remove (k,v) tuple with minimum key.
+        :return:
+        """
+        p = self._find_min()
+        item = p.element()
+        return (item._key, item._value)
+
+    def __len__(self):
+        """
+        Return the number of items in the priority queue.
+        :return:
+        """
+        return len(self._data)
+
+    def add(self, key, value):
+        """
+        Add a key-value pair.
+        :param key:
+        :param value:
+        :return:
+        """
+        self._data.add_last(self._Item(key, value))
+
+    def remove_min(self):
+        """
+        Remove and return (k,v) tuple with minimum key.
+        :return:
+        """
+        p = self._find_min()
+        item = self._data.delete(p)
+        return (item._key, item._value)
