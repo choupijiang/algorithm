e2bdc9945b3be8f874fed254ef70127f973df0fe
choupijiang <zhangshenghu@gmail.com>
2015-02-04 13:07:17 +0800

    modify  sort table map

--- src/MapHashtableSkiplist/SortedTableMap.py
+++ src/MapHashtableSkiplist/SortedTableMap.py
@@ -57,3 +57,107 @@ class SortedTableMap(MapBase):
         :return:
         """
         j = self._find_index(k, 0 , len(self._table) - 1)
+        if  j == len(self._table) or self._table[j]._key != k:
+            raise KeyError('Key Error: ' + repr(k))
+        return self._table[j]._value
+
+    def __setitem__(self, k, v):
+        """
+        Assign value v to key k, overwriting existing value if present.
+        :param k:
+        :param v:
+        :return:
+        """
+        j = self._find_index(k, 0, len(self._table) - 1)
+        if  j < len(self._table) or self._table[j]._key == k:
+            self._table[j]._value = v
+        else:
+            self._table.index(j ,self._Item(k, v))
+
+    def __delitem__(self, k):
+        """
+        Remove item associated with k (raise KeyError if not found.)
+        :param k:
+        :return:
+        """
+        j = self._find_index(k, 0, len(self._table) - 1)
+        if  j == len(self._table) or self._table[j]._key != k:
+            raise KeyError('Key Error: ' + repr(k))
+        self._table.pop(j)
+
+    def __iter__(self):
+        """
+        Generate keys of the map ordered from minimum to maximum.
+        :return:
+        """
+        for item in self._table:
+            yield item._key
+
+    def __reversed__(self):
+        """
+        Generate keys of the map ordered from maximum to minimum
+        :return:
+        """
+        for item in reversed(self._table):
+            yield item._key
+
+    def find_min(self):
+        """
+        Return (key,value) pair with minimum key (or None if empty).
+        :return:
+        """
+        if len(self._table) > 0:
+            return (self._table[0]._key, self._table[0]._value)
+        else:
+            return None
+
+    def find_max(self):
+        """
+        Return (key,value) pair with maximum key (or None if empty)
+        :return:
+        """
+        if len(self._table) > 0:
+            return (self._table[-1]._key, self._table[-1]._value)
+        else:
+            return None
+
+    def find_ge(self, k):
+        """
+        Return (key,value) pair with least key greater than or equal to k.
+        :param k:
+        :return:
+        """
+        j = self._find_index(k, 0, len(self._table) - 1)
+        if j < len(self._table):
+            return (self._table[j]._key, self._table[j]._value)
+        else:
+            return None
+
+    def find_lt(self, k):
+        """
+        Return (key,value) pair with greatest key strictly less than k.
+        :param k:
+        :return:
+        """
+        j = self._find_index(k, 0, len(self._table) - 1)
+        if j > 0:
+            return (self._table[j - 1]._key, self._table[j - 1]._value)
+        else:
+            return None
+
+    def find_range(self, start, stop):
+        """
+        Iterate all (key,value) pairs such that start <= key < stop.
+        If start is None, iteration begins with minimum key of map.
+        If stop is None, iteration continues through the maximum key of map.
+        :param start:
+        :param stop:
+        :return:
+        """
+        if start is None:
+            j = 0
+        else:
+            j = self._find_index(start, 0 ,len(self._table) - 1)
+        while j < len(self._table) and (stop is None or self._table[j]._key < stop):
+            yield (self._table[j]._key, self._table[j]._value)
+            j += 1
