cd35958d30a029e56c43c637fd6c35c2006b198a
choupijiang <zhangshenghu@gmail.com>
2015-03-06 18:38:08 +0800

    add sort selection

--- src/SortSelect/Sort.py
+++ src/SortSelect/Sort.py
@@ -8,7 +8,7 @@ from LinkedQueue import LinkedQueue
 # ------------------Array-Based Implementation of Merge-Sort----------------------
 # def merge(S1, S2, S):
 # """
-#     Merge two sorted Python lists S1 and S2 into properly sized list S.
+# Merge two sorted Python lists S1 and S2 into properly sized list S.
 #     :param S1:
 #     :param S2:
 #     :param S:
@@ -126,4 +126,14 @@ def inplace_quick_sort(S, a, b):
     pivot = S[b]
     left = a
     right = b - 1
-    while left < = right:
+    while left <= right:
+        while left <= right and S[left] < pivot:
+            left += 1
+        while left <= right and pivot < S[right]:
+            right -= 1
+        if left <= right:
+            S[left], S[right] = S[right], S[left]
+            left, right = left + 1, right - 1
+    S[left], S[b] = S[b], S[left]
+    inplace_quick_sort(S, a, left - 1)
+    inplace_quick_sort(S, left + 1, b)
